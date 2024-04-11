2b27d84e0379bbbee89b7fc8d381d20a61502671
okada-taisho <taisho.okada22@gmail.com>
2024-04-11 09:54:26 +0900

    lib/admin修正

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -3,22 +3,23 @@ import { getAuth } from "firebase-admin/auth";
 import admin from "firebase-admin";
 
 const certification: any = {
-  type: process.env.GCP_TYPE,
-  project_id: process.env.GCP_PROJECT_ID,
-  private_key_id: process.env.GCP_PRIVATE_KEY_ID,
-  private_key: process.env.GCP_PRIVATE_KEY?.split(String.raw`\n`).join("\n"),
-  client_email: process.env.GCP_CLIENT_EMAIL,
-  client_id: process.env.GCP_AUTH_URI,
-  auth_uri: process.env.GCP_TOKEN_URI,
-  token_uri: process.env.GCP_AUTH_PROVIDER_X509_CERT_URL,
-  auth_provider_x509_cert_url: process.env.GCP_CLIENT_X509_CERT_URL,
-  client_x509_cert_url: process.env.GCP_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.GCP_UNIVERSE_DOMAIN,
+  type: process.env.NEXT_PUBLIC_GCP_TYPE,
+  project_id: process.env.NEXT_PUBLIC_GCP_PROJECT_ID,
+  private_key_id: process.env.NEXT_PUBLIC_GCP_PRIVATE_KEY_ID,
+  private_key: process.env.NEXT_PUBLIC_GCP_PRIVATE_KEY?.split(String.raw`\n`).join("\N"),
+  client_email: process.env.NEXT_PUBLIC_GCP_CLIENT_EMAIL,
+  client_id: process.env.NEXT_PUBLIC_GCP_AUTH_URI,
+  auth_uri: process.env.NEXT_PUBLIC_GCP_TOKEN_URI,
+  token_uri: process.env.NEXT_PUBLIC_GCP_AUTH_PROVIDER_X509_CERT_URL,
+  auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_GCP_CLIENT_X509_CERT_URL,
+  client_x509_cert_url: process.env.NEXT_PUBLIC_GCP_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.NEXT_PUBLIC_GCP_UNIVERSE_DOMAIN,
 };
 
+let app;
 
-export const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert(certification),
-});
-
-export const auth = getAuth();
+if (admin.apps.length === 0) {
+  app = initializeApp(certification);
+}
+console.log(app);
+export const auth = getAuth(app);
